61f9223e9b82816062cd03d73b21658f41d342f5
Berzan <berzan@tutanota.com>
2023-10-11 10:34:03 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === "production" ? "/nft-pawn-shop" : undefined,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
