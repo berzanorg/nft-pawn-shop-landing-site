a68506960fbf04109aa83323fae1187124d7d79d
Berzan <berzan@tutanota.com>
2023-10-20 13:51:17 +0000

    import: `uikit.min.css` file

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import '../assets/js/anime-helper.js';
 import '../assets/js/swiper-helper.js';
 import '../assets/js/typed-helper.js';
 
+import '../assets/css/uikit.min.css';
+
 import React, { useEffect } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -18,33 +20,33 @@ function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = React.useState(false);
   const [firstRender, setFirstRender] = React.useState(false);
 
-  useEffect(() => {  
+  useEffect(() => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
         const hrefValue = this.getAttribute('href');
-        
+
         // "#" bağlantıları için özel bir kontrol ekleyin, 
         // böylece bunlar üzerinde querySelector çalıştırmayın
         if (hrefValue === '#') {
           e.preventDefault(); // Varsayılan tıklama işlemini engelle
           return; // Hedef yok, bu yüzden işleyiciden çık
         }
-    
+
         const target = document.querySelector(hrefValue);
-    
+
         if (!target) return;
-    
+
         e.preventDefault();
         target.scrollIntoView({
           behavior: 'smooth'
         });
       });
     });
-    
-    
+
+
   })
 
-  React.useEffect(() =>{    
+  React.useEffect(() => {
     setFirstRender(true);
   })
 
@@ -53,10 +55,10 @@ function MyApp({ Component, pageProps }) {
   }
   return (
     <>
-   
+
       <React.Fragment>
-          <Component {...pageProps} />
-        </React.Fragment>
+        <Component {...pageProps} />
+      </React.Fragment>
     </>
   )
 }
